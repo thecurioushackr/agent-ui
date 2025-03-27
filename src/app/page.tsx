dacf39f4667cc9556ab9f42303809d64b38ee7ac
ansub <khanansub3@gmail.com>
2025-03-27 13:54:01 +0400

    Wrap Home component in Suspense for improved loading state management

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+'use client'
 import Sidebar from '@/components/playground/Sidebar/Sidebar'
 import { ChatArea } from '@/components/playground/ChatArea'
+import { Suspense } from 'react'
 
 export default function Home() {
   return (
-    <div className="bg-background/80 flex h-screen">
-      <Sidebar />
-      <ChatArea />
-    </div>
+    <Suspense>
+      <div className="bg-background/80 flex h-screen">
+        <Sidebar />
+        <ChatArea />
+      </div>
+    </Suspense>
   )
 }
